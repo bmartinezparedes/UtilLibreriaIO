5e3ae8046ac6fa240f15fd72c8c6d7931d4c6112
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-02-26 08:54:42 +0100

    Actualizo 2 LibreriaIN_OUT en mi trabajo

--- src/cod/Main.java
+++ src/cod/Main.java
@@ -1,8 +1,14 @@
 package cod;
+import cod.metodos.PorTerminal;
+import cod.metodos.PorVentana;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        PorTerminal pt=new PorTerminal("Hola");
+        pt.visualiza();
+        PorVentana pv=new PorVentana("Adios");
+        pv.visualiza();
     }
 }
