58ccc1375afab29727e9d80db0203256b24a7b3a
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-03-26 09:11:15 +0100

    Libreria

--- src/cod/Main.java
+++ src/cod/Main.java
@@ -1,14 +1,23 @@
 package cod;
+
+import cod.metodos.FactoriaDeSalidas;
+import cod.metodos.InterfacesMetodosComunes;
 import cod.metodos.PorTerminal;
 import cod.metodos.PorVentana;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        PorTerminal pt=new PorTerminal("Hola");
-        pt.visualiza();
-        PorVentana pv=new PorVentana("Adios");
-        pv.visualiza();
+        // write your code here
+        InterfacesMetodosComunes miSalidaT;
+        InterfacesMetodosComunes miSalidaV;
+        InterfacesMetodosComunes miSalidaI;
+        miSalidaT = FactoriaDeSalidas.getProducto(1, "Hola desde la fabrica");
+        miSalidaT.visualiza();
+        miSalidaV = FactoriaDeSalidas.getProducto(2, "Adios desde la factoria");
+        miSalidaV.visualiza();
+        miSalidaI = FactoriaDeSalidas.getProducto(3, "Por Impresora");
+        miSalidaI.visualiza();
+
     }
 }
